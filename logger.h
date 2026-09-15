2fcc913f25d344fafd3807c23acad986c9021c46
wh13371 <wh13371@users.noreply.github.com>
2026-09-15 14:58:29 +0100

    Update logger.h
    added log levels & log file

--- logger.h
+++ logger.h
@@ -1,14 +1,23 @@
 #ifndef LOGGER_H
 #define LOGGER_H
 
-#include <stdio.h>       // printf()
-#include <time.h>       // localtime()
-#include <stdarg.h>     // va_list - variable args
-#include <unistd.h>     // getpid()
-#include <sys/time.h>   // gettimeofday()
+#define LOG_MAX_LEN    2048
 
-#define LOG_MAX_LEN    1024
+typedef enum {
+    LOG_DEBUG,
+    LOG_INFO,
+    LOG_WARN,
+    LOG_ERROR
+} LogLevel;
 
-void LOG(const char *fmt, ...);
+void set_log_level(LogLevel log_level);
+int set_log_filename(const char *log_filename);
+void _LOG(LogLevel log_level, const char *file, int line, const char *fmt, ...);
+void close_logger();
+
+#define LOG_INFO(fmt, ...)  _LOG(LOG_INFO,  __FILE__, __LINE__, fmt, ##__VA_ARGS__)
+#define LOG_DEBUG(fmt, ...) _LOG(LOG_DEBUG, __FILE__, __LINE__, fmt, ##__VA_ARGS__)
+#define LOG_WARN(fmt, ...)  _LOG(LOG_WARN,  __FILE__, __LINE__, fmt, ##__VA_ARGS__)
+#define LOG_ERROR(fmt, ...) _LOG(LOG_ERROR, __FILE__, __LINE__, fmt, ##__VA_ARGS__)
 
 #endif // LOGGER_H
